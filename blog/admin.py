a617f0539002c0cbd8da5507c87125b88e4a7719
amasomo <gutenfair@gmail.com>
2019-07-15 16:50:48 -0500

    Pubished time and New post

--- blog/admin.py
+++ blog/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from .models import Post, Comment
 from .models import Post
 
 admin.site.register(Post) #This is for making our model visible on the admin page
+admin.site.register(Comment)
